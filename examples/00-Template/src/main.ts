bc904b77f01f394fce9946f2aa3f42dc9b47c309
Matthew Ross <kiswa.com@gmail.com>
2019-04-26 16:00:22 -0400

    Update template

--- examples/00-Template/src/main.ts
+++ examples/00-Template/src/main.ts
@@ -1,6 +1,9 @@
 import { Game, Types } from 'kilo/lib'
 
-const game = new Game()
+const game = new Game(320, 240)
+
+// Uncomment below line to show FPS & UPS counters
+// Game.debug = true
 
 game.run()
 
