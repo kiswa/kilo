a82eb27344392d56d68eda2f2b57d6ffbf704d7d
Matthew Ross <kiswa.com@gmail.com>
2019-05-01 14:10:20 -0400

    Add player sprite to Tiled maps example

--- examples/06-TiledMaps/src/main.ts
+++ examples/06-TiledMaps/src/main.ts
@@ -1,19 +1,25 @@
-import { Game, Types, Utils } from 'kilo/lib'
+import { Game, TileSprite, Types, Utils } from 'kilo/lib'
 
 import { TiledLevel } from './tiled-map'
 
 // Uncomment below line to show FPS & UPS counters
 // Game.debug = true
 
-let game: Game
-let gameMap: TiledLevel
-
 const initGame = (mapData: any) => {
   const tMap = Utils.tiledParser(mapData, ['bg'])
   const map = new TiledLevel(tMap)
 
-  game = new Game(map.mapWidth * map.tileWidth, map.mapHeight * map.tileHeight)
-  gameMap = game.scene.add<TiledLevel>(map)
+  const game = new Game(map.mapWidth * map.tileWidth, map.mapHeight * map.tileHeight)
+  const gameMap = game.scene.add<TiledLevel>(map)
+
+  const playerTex = new Types.Texture('assets/images/player.png')
+  const player = new TileSprite(playerTex, 40, 55)
+
+  player.pos.set(gameMap.gameObjects.player.x,
+    gameMap.gameObjects.player.y - 23)
+  player.frame.x = 2
+
+  game.scene.add(player)
 
   game.run()
 }
