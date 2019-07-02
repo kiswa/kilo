5d3877d4f46bf2203bd79bb6251a6d6d90809b21
Matthew Ross <kiswa.com@gmail.com>
2019-07-02 15:28:33 -0400

    Update example to use all three sound options

--- examples/08-Sounds/src/main.ts
+++ examples/08-Sounds/src/main.ts
@@ -3,27 +3,36 @@ import { Game, Sound, SoundGroup, SoundPool, KeyControls, Types } from 'kilo/lib
 const game = new Game(320, 240)
 const controls = new KeyControls()
 
-let gameType = 'coin'
-
 // Uncomment below line to show FPS & UPS counters
-Game.debug = true
+// Game.debug = true
 
 // Although the update loop ensures the sound can't play unless .3 seconds
 // have elapsed, it can still cause the existing sound to stop partway through.
 // To allow overlapping sounds, use a SoundPool (see below).
 const coin = new Sound('../assets/sounds/coin.ogg')
-const coinText = new Types.Text('Press A to play coin sound.', {
-  font: '20px monospace',
+const coinTextA = new Types.Text('Press A to play coin sound.', {
+  font: '14px monospace',
+  fill: '#fff'
+})
+const coinTextS = new Types.Text('Press S to play coin sound from pool.', {
+  font: '14px monospace',
+  fill: '#fff'
+})
+const coinTextD = new Types.Text('Press D to play coin sound from group.', {
+  font: '14px monospace',
   fill: '#fff'
 })
 
-game.scene.add(coinText)
+coinTextS.pos.y = 20
+coinTextD.pos.y = 40
 
-// Change gameType to 'coinPool' to enable the sound pool.
+game.scene.add(coinTextA)
+game.scene.add(coinTextS)
+game.scene.add(coinTextD)
+
 const coinPool = new SoundPool('assets/sounds/coin.ogg')
 let lastRun = 0
 
-// Change gameType to 'soundGroup' to enable random sound playing.
 const coinGroup = new SoundGroup([
   new Sound('../assets/sounds/coin.ogg'),
   new Sound('../assets/sounds/coin2.ogg'),
@@ -31,23 +40,17 @@ const coinGroup = new SoundGroup([
 ])
 
 game.run((dt: number, t: number) => {
-  // Kind of cheating here since A is move left by default
-  if (controls.x === -1 && t - lastRun > .3) {
-    switch (gameType) {
-      case 'coin':
-        coin.play()
-        break
-
-      case 'coinPool':
-        coinPool.play()
-        break
-
-      case 'coinGroup':
-        coinGroup.play()
-        break
-    }
-
+  if (controls.key('KeyA') && t - lastRun > .3) {
+    coin.play()
     lastRun = t
+  }
+
+  if (controls.key('KeyS')) {
+    coinPool.play()
+  }
+
+  if (controls.key('KeyD')) {
+    coinGroup.play()
   }
 })
 
