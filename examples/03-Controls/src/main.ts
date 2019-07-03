ca528e251226a1dbba45bc71e1078f4a4def7a91
Matthew Ross <kiswa.com@gmail.com>
2019-07-03 08:44:27 -0400

    Include mouse controls in example

--- examples/03-Controls/src/main.ts
+++ examples/03-Controls/src/main.ts
@@ -1,4 +1,5 @@
-import { Game, KeyControls, Types } from 'kilo/lib'
+import { Game, Controls, KeyControls, MouseControls, Types } from 'kilo/lib'
+import { math } from 'kilo/lib/utils'
 
 const tileSize = 32
 const mapSize = tileSize * 13
@@ -8,17 +9,30 @@ const game = new Game(mapSize, mapSize)
 // Game.debug = true
 
 const plane = new Types.Sprite(new Types.Texture('assets/images/plane.png'))
-const controls = new KeyControls()
+const controls: Controls = {
+  keys: new KeyControls(),
+  mouse: new MouseControls(game.canvas)
+}
 
 const speed = 0.2
 
 game.scene.add(plane)
 
 game.run((dt: number, t: number) => {
-  const { x, y } = controls
+  const { x, y } = controls.keys
+  const { mouse } = controls
 
   plane.pos.x += x * dt * (tileSize / speed)
   plane.pos.y += y * dt * (tileSize / speed)
+
+  mouse.update()
+
+  if (mouse.isDown) {
+    const angle = math.angle(mouse.pos, plane.pos)
+    const dir = math.dirTo(angle)
+
+    plane.pos.add(dir.multiply(4))
+  }
 
   // Keep the sprite inside the view
   if (plane.pos.x < -8) {
